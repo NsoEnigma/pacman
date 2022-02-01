1e10c6211d74c6c5f068ef90e9d4073219aa0493
NsoEnigma <alexcelia910@gmail.com>
2022-02-01 19:48:58 +0100

    solo falta powerUP

--- src/main/java/paquete/Funciones.java
+++ src/main/java/paquete/Funciones.java
@@ -1,40 +1,274 @@
 package paquete;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Funciones {
 
 	public static char[] arrayMonedas(byte tam) {
-		char[] tablero=new char[tam];
-		for(byte i=0;i<tablero.length;i++) {
-			tablero[i]='�';
-		}
-		return tablero;
-	}
-	
-	public static String imprimeArray(char[] arr) {
-		String ret="[ ";
-		for(char el:arr) {
-			ret+=el+" ";
-		}
-		return ret+"]";
-	}
-	
-	/**
-	 * Funci�n que coloca al personaje elegido en una de las posiciones
-	 * del array, devuelve la posici�n en la que se ha colocado el personaje.
-	 * @param tablero array donde se coloca el personaje. La posici�n elegida queda modificada
-	 * @param personaje char que representa al personaje que se coloca
-	 * @return posici�n del array en la que se ha colocado el personaje
-	 */
-	public static byte colocaPersonaje(char[] tablero,char personaje) {
-		Random r=new Random();
-		byte pos=(byte)r.nextInt(tablero.length);
-		if(tablero[pos]!='�') {
-			return colocaPersonaje(tablero,personaje);
-		}
-		tablero[pos]=personaje;
-		return pos;
-	}
-	
+		
+		char[] tablero = new char[tam];
+		
+		for (byte i = 0; i < tablero.length; i++) {
+		 
+		 			tablero[i] = '©';
+		 
+		 		}
+		 
+		 		return tablero;
+		 
+		 	}
+		 
+		 	public static String imprimeArray(char[] arr) {
+		 
+		 		String ret = "[ ";
+		 
+		 		for (char el : arr) {
+		 
+		 			ret += el + " ";
+		 
+		 		}
+		 
+		 		return ret + "]";
+		 
+		 	}
+		 
+		 	/**
+		 	 * 
+		 	 * Función que coloca al personaje elegido en una de las posiciones
+		 	 * 
+		 	 * del array, devuelve la posición en la que se ha colocado el personaje.
+		 	 * 
+		 	 * @param tablero   array donde se coloca el personaje. La posición elegida
+		 	 *                  queda modificada
+		 	 * 
+		 	 * @param personaje char que representa al personaje que se coloca
+		 	 * 
+		 	 * @return posición del array en la que se ha colocado el personaje
+		 	 * 
+		 	 */
+		 
+		 	public static byte colocaPersonaje(char[] tablero, char personaje) {
+		 
+		 		Random r = new Random();
+		 
+		 		byte pos = (byte) r.nextInt(tablero.length);
+		 
+		 		if (tablero[pos] != '©') {
+		 
+		 			return colocaPersonaje(tablero, personaje);
+		 
+		 		}
+		 
+		 		tablero[pos] = personaje;
+		 
+		 		return pos;
+		 
+		 	}
+		 
+		 	/**
+		 	 * 
+		 	 * Función que mueve un personaje en un tablero, haciendo
+		 	 * 
+		 	 * movimiento circular en los casos extremo. Devuelve concatenadas
+		 	 * 
+		 	 * y separadas por # la nueva posición del personaje y lo que había
+		 	 * 
+		 	 * anteriormente en la casilla a la que el personaje se ha movido
+		 	 * 
+		 	 * @param tablero       array en el que los personajes se mueven
+		 	 * 
+		 	 * @param personaje     representación en char del personaje que se mueve
+		 	 * 
+		 	 * @param posPersonaje  posición inicial donde se encuentra el personaje
+		 	 * 
+		 	 * @param direccion     'a' para mover a la izda, o 'd' para la derecha.
+		 	 *                      Cualquier otro es no válido.
+		 	 * 
+		 	 * @param queHabiaAntes elemento que había en posPersonaje antes de que
+		 	 *                      personaje llegase ahí, para restaurarlo tras su
+		 	 *                      movimiento
+		 	 * 
+		 	 * @return un String con la nueva posición del personaje y lo que había en la
+		 	 *         posición a la que el personaje se ha movido, concatenados y separados
+		 	 *         por #
+		 	 * 
+		 	 */
+		 
+		 	public static String moverPersonaje(
+		 
+		 			char[] tablero, char personaje, byte posPersonaje,
+		 
+		 			char direccion, char queHabiaAntes) {
+		 
+		 		tablero[posPersonaje] = queHabiaAntes;
+		 
+		 		switch (direccion) {
+		 
+		 		case 'a':
+		 
+		 			if (posPersonaje > 0) {
+		 
+		 				posPersonaje--;
+		 
+		 			} else {
+		 
+		 				posPersonaje = (byte) (tablero.length - 1);
+		 
+		 			}
+		 
+		 			break;
+		 
+		 		case 'd':
+		 
+		 			if (posPersonaje < tablero.length - 1) {
+		 
+		 				posPersonaje++;
+		 
+		 			} else {
+		 
+		 				posPersonaje = 0;
+		 
+		 			}
+		 
+		 			break;
+		 
+		 		default:
+		 
+		 			System.out.println("Entrada no válida");
+		 
+		 		}
+		 		// Si en la posición a la que va hay un fantasma, pone un carácter especial &
+		 		// para no restaurar el fantasma al moverse del nuevo sitio
+		 		char loQueHay = (tablero[posPersonaje] == 'ᗣ' ? '&' : tablero[posPersonaje]);
+		 
+		 		tablero[posPersonaje] = personaje;
+		 
+		 		return posPersonaje + "#" + loQueHay;
+		 
+		 	}
+		 
+		 	/**
+			 * 
+			 * Función que mueve un personaje en un tablero, haciendo
+			 * 
+			 * movimiento circular en los casos extremo. Devuelve concatenadas
+			 * 
+			 * y separadas por # la nueva posición del personaje y lo que había
+			 * 
+			 * anteriormente en la casilla a la que el personaje se ha movido
+			 * 
+			 * @param tablero       array en el que los personajes se mueven
+			 * 
+			 * @param personaje     representación en char del personaje que se mueve
+			 * 
+			 * @param posPersonaje  posición inicial donde se encuentra el personaje
+			 * 
+			 * @param direccion     'a' para mover a la izda, o 'd' para la derecha.
+			 *                      Cualquier otro es no válido.
+			 * 
+			 * @param queHabiaAntes elemento que había en posPersonaje antes de que
+			 *                      personaje llegase ahí, para restaurarlo tras su
+			 *                      movimiento
+			 * 
+			 * @return un String con la nueva posición del personaje y lo que había en la
+			 *         posición a la que el personaje se ha movido, concatenados y separados
+			 *         por #
+			 * 
+			 */
+
+			public static String moverFantasma(
+
+					char[] tablero, char personaje, byte posPersonaje,
+
+					char direccion, char queHabiaAntes) {
+
+				tablero[posPersonaje] = queHabiaAntes;
+
+				switch (direccion) {
+
+				case 'a':
+
+					if (posPersonaje > 0) {
+
+						posPersonaje--;
+
+					} else {
+
+						posPersonaje = (byte) (tablero.length - 1);
+
+					}
+
+					break;
+
+				case 'd':
+
+					if (posPersonaje < tablero.length - 1) {
+
+						posPersonaje++;
+
+					} else {
+
+						posPersonaje = 0;
+
+					}
+
+					break;
+
+				default:
+
+					System.out.println("Entrada no válida");
+
+				}
+
+				char loQueHay = tablero[posPersonaje];
+
+				tablero[posPersonaje] = personaje;
+
+				return posPersonaje + "#" + loQueHay;
+
+			}
+
+			public static String imprimeArray1(char[] arr) {
+				String ret = "";
+				for (char el : arr) {
+					ret += el + " ";
+
+				}
+				return ret + " ";
+			}
+
+			public static String imprimeArray(String[] arr) {
+				String ret = "";
+				for (String el : arr) {
+					ret += el + " ";
+
+				}
+				return ret + " ";
+
+			}
+
+			public static String imprimeArray(float[] arr) {
+				String ret = "";
+				for (float el : arr) {
+					ret += el + " ";
+
+				}
+				return ret + " ";
+			}
+
+			public static String[] creaArrayStrings() {
+
+				Scanner lector = new Scanner(System.in);
+				System.out.println("Dime un nemero");
+				int h = Integer.parseInt(lector.nextLine());
+				String[] arr = new String[h];
+				for (int i = 0; i < h; i++) {
+					System.out.println("Dime un nombre para esta posicion");
+					arr[i] = lector.nextLine();
+
+				}
+				return arr;
+
+			}
 }
--- src/main/java/paquete/Pacman.java
+++ src/main/java/paquete/Pacman.java
@@ -1,57 +1,149 @@
 package paquete;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Pacman {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+
+		Scanner sc = new Scanner(System.in);
+
 		byte tamanio;
+
 		do {
+
 			System.out.println("Dime el tamaño del array (entre 3 y 64)");
-			tamanio=Byte.parseByte(sc.nextLine());
-		}while(tamanio<3||tamanio>64);
 
-		char[] tablero=Funciones.arrayMonedas(tamanio);
-		
-		byte posPacman=Funciones.colocaPersonaje(tablero,'ᗧ');
-		byte posFantasma=Funciones.colocaPersonaje(tablero,'ᗣ');
-		byte monedas=(byte)(tablero.length-2);
-		
-		System.out.println("Monedas: "+monedas);
+			tamanio =  Byte.parseByte(sc.nextLine());
+			if(tamanio < 4 || tamanio > 64) {System.out.println("Entrada no válida");};
+
+		} while (tamanio < 4 || tamanio > 64);
+		char[] tablero = Funciones.arrayMonedas(tamanio);
+
+		byte posPacman = Funciones.colocaPersonaje(tablero, 'ᗧ');
+
+		byte posFantasma = Funciones.colocaPersonaje(tablero, 'ᗣ');
+
+		byte posFantasma2 = Funciones.colocaPersonaje(tablero, 'ᗣ');
+
+		byte monedas = (byte) (tablero.length - 1);
+
+		System.out.println("Monedas: " + monedas);
+
 		System.out.println(Funciones.imprimeArray(tablero));
-		
-		do {
-			System.out.println("¿Dónde te mueves a | d ?");
-			char dondeVoy=sc.nextLine().toLowerCase().charAt(0);
-			
-			tablero[posPacman]=' ';
-			switch(dondeVoy) {
-			case 'a':
-				posPacman--;
-			break;
-			case 'd':
-				posPacman++;
-			break;
-			default:
-				System.out.println("Entrada no válida");
-			}
-			if(tablero[posPacman]=='©') {
-				monedas--;
-			}
-			tablero[posPacman]='ᗧ';
-			
-			System.out.println("\nMonedas: "+monedas);
-			System.out.println(Funciones.imprimeArray(tablero));
-		}while(posPacman!=posFantasma&&monedas>0);
-		
-		
-		if(posPacman!=posFantasma) {
-			System.out.println("¡Enhorabuena, has ganado!");
-		}else {
-			System.out.println("El fantasma te ha comido");
-		}
-	}
 
+		char trasElFantasma = '©';
+		char trasElFantasma2 = '©';
+		do {
+
+			System.out.println("¿Dónde te mueves a | d ?");
+
+			char dondeVoy = sc.nextLine().toLowerCase().charAt(0);
+
+			// Movimiento del pacman
+
+			String paraSplit = Funciones.moverPersonaje(tablero, 'ᗧ',
+
+					posPacman, dondeVoy, ' ');
+
+			String[] posYAntes = paraSplit.split("#");
+
+			posPacman = Byte.parseByte(posYAntes[0]);
+
+			if (posYAntes[1].charAt(0) == '©') {
+
+				monedas--;
+
+			}
+
+			// compruebo si pacman ha caido donde el fantasma
+
+			if (posPacman == posFantasma) {
+
+				break;
+
+			}
+
+			// Movimiento 2º fantasma
+
+			String paraSplit2 = Funciones.moverPersonaje(tablero, 'ᗣ',
+
+					posFantasma2, dondeVoy, trasElFantasma2);
+
+			String[] posYAntes2 = paraSplit2.split("#");
+			posFantasma2 = Byte.parseByte(posYAntes2[0]);
+			if (posYAntes2[1].charAt(0) != '&') {
+				trasElFantasma2 = posYAntes2[1].charAt(0);
+			} else {
+				trasElFantasma2 = trasElFantasma;
+				trasElFantasma = 'ᗣ';
+			}
+			/**
+			 * if (posPacman == posFantasma2) {
+			 * 
+			 * break;
+			 * 
+			 * }
+			 **/
+
+			// Estrategia del fantasma , la contraria de donde se mueva pacman
+
+			// Al contrario que tu
+
+			/*
+			 * char dondeVoyFantasma='a';
+			 * 
+			 * if(dondeVoy=='a') {
+			 * 
+			 * dondeVoyFantasma='d';
+			 * 
+			 * }
+			 */
+
+			// Aleatoriamente
+
+			Random r = new Random();
+
+			char[] movimientos = { 'a', 'd' };
+
+			char dondeVoyFantasma =
+
+					movimientos[r.nextInt(movimientos.length)];
+
+			// Movimiento del fantasma
+
+			paraSplit = Funciones.moverFantasma(tablero, 'ᗣ', posFantasma,
+
+					dondeVoyFantasma, trasElFantasma);
+
+			posYAntes = paraSplit.split("#");
+
+			posFantasma = Byte.parseByte(posYAntes[0]);
+
+			if (posYAntes[1].charAt(0) != '&') {
+				trasElFantasma = posYAntes[1].charAt(0);
+			} else {
+				trasElFantasma = trasElFantasma2;
+				trasElFantasma2 = 'ᗣ';
+			}
+
+			System.out.println("\nMonedas: " + monedas);
+
+			System.out.println(Funciones.imprimeArray(tablero));
+
+		} while (posPacman != posFantasma && monedas > 0);
+
+		if (posPacman != posFantasma) {
+
+			System.out.println("¡Enhorabuena, has ganado!");
+
+		} else {
+
+			System.out.println("El fantasma te ha comido");
+
+		}
+
+	}
 }
 
